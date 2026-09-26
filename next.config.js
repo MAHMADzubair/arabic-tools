6a0f83ec7936769ebc9d7b88b80479b442de6455
MAHMADzubair <168504647+MAHMADzubair@users.noreply.github.com>
2026-09-27 01:10:58 +0500

    Add redirect from /final-settlement-calculator to /ar/sa/final-settlement-calculator

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/final-settlement-calculator",
+        destination: "/ar/sa/final-settlement-calculator",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
